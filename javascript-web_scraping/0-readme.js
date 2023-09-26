f5c1a28bc308c343d284b48440a08e87aeb3045f
jGohan-cpu <jjoseriveram01@gmail.com>
2023-09-25 22:55:38 -0400

    0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
+/* eslint-disable */
 
-// Import the 'fs' (file system) module to work with files
 const fs = require('fs');
 
-// Use 'fs.readFile' to read the content of the file specified as the command-line argument
-// 'process.argv[2]' is the third argument, which is expected to be the filename
-// 'utf8' specifies the encoding of the file (UTF-8)
-// The provided callback function will be executed when the file is read
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-    // Check if an error occurred while reading the file
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
     if (err) {
-        console.log(err); // Print the error message to the console
+        console.log(err);
     } else {
-        // If there's no error, write the file's content to the standard output (console)
-        process.stdout.write(data);
+        console.log(data);
     }
 });
