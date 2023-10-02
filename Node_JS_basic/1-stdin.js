6628e4c01e0b84b62522e0e44de1b64cd8678c32
jGohan-cpu <jjoseriveram01@gmail.com>
2023-10-02 17:18:25 -0400

    adjusted code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,18 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input) => {
-    console.log('Your name is: ${input}');
-    console.log('This important software is now closing');
-    rl.close();
-})
+rl.on('line', (name) => {
+    if (name) {
+        console.log(`Your name is: ${name}`);
+        console.log('This important software is now closing');
+        rl.close();
+    } else {
+        console.log('Please enter your name:');
+    }
+});
