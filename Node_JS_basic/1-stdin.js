b72841a48409595a55571d9a4fa5b2fff008ff49
jGohan-cpu <jjoseriveram01@gmail.com>
2023-10-11 17:32:09 -0400

    spaces change

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,15 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (name) => {
-    if (name) {
-        console.log(`Your name is: ${name}`);
-        console.log('This important software is now closing');
-        rl.close();
-    } else {
-        console.log('Please enter your name:');
-    }
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
